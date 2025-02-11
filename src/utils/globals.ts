f806ebca1834eecfdfdf2557309f9f5d4b74548a
Tom Seifert <dev@tpseifert.de>
2025-02-11 13:56:51 +0100

    Imprint

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,2 +1,3 @@
 export const contactEmail = import.meta.env.PUBLIC_CONTACT_EMAIL ?? '';
 export const contactPhone = import.meta.env.PUBLIC_CONTACT_PHONE ?? '';
+export const contactAddressBase64 = import.meta.env.PUBLIC_CONTACT_ADDRESS ?? '';
